15fc1f7be5e594fe93f4c74a7f8ccaf5340e597a
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2017-07-26 13:41:54 +0530

    Debug Permutation.java

--- Week 2/Permutation.java
+++ Week 2/Permutation.java
@@ -2,20 +2,15 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Permutation {
-	private static RandomizedQueue<String> queue = null;
-
-	private static void initialise() {
-		queue = new RandomizedQueue<>();
-	}
 
 	public static void main(String[] args) {
 		int num = Integer.parseInt(args[0]);
-		initialise();
-		while (StdIn.isEmpty()) {
-			Permutation.queue.enqueue(StdIn.readString());
+		RandomizedQueue<String> queue = new RandomizedQueue<>();
+		while (!StdIn.isEmpty()) {
+			queue.enqueue(StdIn.readString());
 		}
 		for (int i = 0; i < num; i++) {
-			StdOut.println(Permutation.queue.dequeue());
+			StdOut.println(queue.dequeue());
 		}
 
 	}
