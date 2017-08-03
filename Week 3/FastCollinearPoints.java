3147760f05eda9d652aaf118ee63037bc260cecd
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2017-08-03 15:30:20 +0530

    Week 3 : Add Code to FastCollinearPoints.java

--- Week 3/FastCollinearPoints.java
+++ Week 3/FastCollinearPoints.java
@@ -1,9 +1,36 @@
 package week3;
 
 public class FastCollinearPoints {
-
+	
+	private final Point[] points;
+	private int numberOfSegments=0;
+	
+	FastCollinearPoints(Point[] points){
+		if(points==null){
+			throw new IllegalArgumentException();
+		}
+		int length=points.length;
+		for(int i=0;i<length=i++){
+			if(points[i]==null){
+				throw new IllegalArgumentException();
+			}
+		}
+		this.points=points;
+	}
+	
+	public void numberOfSegments(){
+		return numberOfSegments;
+	}
+	
+	public LineSegment[] segments(){
+		int length=points.length;
+		for(int i=0;i<length;i++){
+			Arrays.sort(points,points[i].slopeOrder());
+			
+		}
+	}
+	
 	public static void main(String[] args) {
-		
 
 	}
 
