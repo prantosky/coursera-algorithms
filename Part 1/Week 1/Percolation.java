f656e0ef65c3174f73ccea64144013e32828f67d
Pranav Chaudhary <pranavchaudhary2310@gmail.com>
2020-12-24 11:22:34 +0530

    Cleaning the code

--- Part 1/Week 1/Percolation.java
+++ Part 1/Week 1/Percolation.java
@@ -2,80 +2,91 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-	private final boolean[][] map;
-	private final WeightedQuickUnionUF grid;
-	private final WeightedQuickUnionUF gridWithoutBottom;
-	private int closedSites;
-	private final int n;
+    private final WeightedQuickUnionUF sites;
+    private final WeightedQuickUnionUF sitesWithoutBottomConnection;
+    private final int n;
+    private int numberOfOpenSites;
+    private final boolean[][] map;
 
-	public Percolation(int n) {	
-		if (n <= 0) {
-			throw new IllegalArgumentException();
-		}
-		map = new boolean[n][n];
-		closedSites = n * n;
-		this.n = n;
-		grid = new WeightedQuickUnionUF(n * n + 2);
-		gridWithoutBottom = new WeightedQuickUnionUF(n * n + 1);
-	}
 
-	public void open(int row, int col) {
-		if (row < 1 || row > n || col < 1 || col > n) {
-			throw new IllegalArgumentException();
-		}
-		if (isOpen(row, col)) {
-			return;
-		}
-		map[row - 1][col - 1] = true;
-		closedSites--;
-		if (row - 1 > 0 && isOpen(row - 1, col)) {
-			grid.union(map(row, col), map(row - 1, col));
-			gridWithoutBottom.union(map(row, col), map(row - 1, col));
-		}
-		if (row + 1 <= n && isOpen(row + 1, col)) {
-			grid.union(map(row, col), map(row + 1, col));
-			gridWithoutBottom.union(map(row, col), map(row + 1, col));
-		}
-		if (col - 1 > 0 && isOpen(row, col - 1)) {
-			grid.union(map(row, col), map(row, col - 1));
-			gridWithoutBottom.union(map(row, col), map(row, col - 1));
-		}
-		if (col + 1 <= n && isOpen(row, col + 1)) {
-			grid.union(map(row, col), map(row, col + 1));
-			gridWithoutBottom.union(map(row, col), map(row, col + 1));
-		}
-		if (row == 1) {
-			grid.union(0, map(row, col));
-			gridWithoutBottom.union(0, map(row, col));
-		}
-		if (row == n) {
-			grid.union(n * n + 1, map(row, col));
-		}
-	}
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        if (n <= 0) throw new IllegalArgumentException();
+        this.sites = new WeightedQuickUnionUF(n * n + 2);
+        this.sitesWithoutBottomConnection = new WeightedQuickUnionUF(n * n + 1);
+        this.n = n;
+        this.numberOfOpenSites = 0;
 
-	public boolean isOpen(int row, int col) {
-		if (row < 1 || row > n || col < 1 || col > n) {
-			throw new IllegalArgumentException();
-		}
-		return map[row - 1][col - 1];
-	}
+        map = new boolean[n][n];
+        for (int i = 1; i <= n; i++) {
+            sites.union(0, i);
+            sitesWithoutBottomConnection.union(0, i);
+        }
+        for (int i = n * n; i > n * n - n; i--) {
+            sites.union(n * n + 1, i);
+        }
+    }
 
-	public boolean isFull(int row, int col) {
-		if (row < 1 || row > n || col < 1 || col > n) {
-			throw new IllegalArgumentException();
-		}
-		return gridWithoutBottom.connected(0, map(row, col));
-	}
+    private int getIndex(int row, int col) {
+        return (row - 1) * n + col;
+    }
 
-	public int numberOfOpenSites() {
-		return n * n - closedSites;
-	}
+    private boolean areNotValidArguments(int row, int col) {
+        return row < 1 || row > n || col < 1 || col > n;
+    }
 
-	public boolean percolates() {
-		return grid.connected(0, n * n + 1);
-	}
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (areNotValidArguments(row, col)) throw new IllegalArgumentException();
+        if (!isOpen(row, col)) {
+            numberOfOpenSites++;
+            map[row - 1][col - 1] = true;
+            if (!areNotValidArguments(row - 1, col) && isOpen(row - 1, col)) {
+                sites.union(getIndex(row, col), getIndex(row - 1, col));
+                sitesWithoutBottomConnection.union(getIndex(row, col), getIndex(row - 1, col));
+            }
+            if (!areNotValidArguments(row, col - 1) && isOpen(row, col - 1)) {
+                sites.union(getIndex(row, col), getIndex(row, col - 1));
+                sitesWithoutBottomConnection.union(getIndex(row, col), getIndex(row, col - 1));
+            }
+            if (!areNotValidArguments(row + 1, col) && isOpen(row + 1, col)) {
+                sites.union(getIndex(row, col), getIndex(row + 1, col));
+                sitesWithoutBottomConnection.union(getIndex(row, col), getIndex(row + 1, col));
+            }
+            if (!areNotValidArguments(row, col + 1) && isOpen(row, col + 1)) {
+                sites.union(getIndex(row, col), getIndex(row, col + 1));
+                sitesWithoutBottomConnection.union(getIndex(row, col), getIndex(row, col + 1));
+            }
+        }
+    }
 
-	private int map(int row, int col) {
-		return (row - 1) * n + col;
-	}
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+//        System.out.println("isOpen: row: " + row + ", col: " + col);
+        if (areNotValidArguments(row, col)) throw new IllegalArgumentException();
+        return map[row - 1][col - 1];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (areNotValidArguments(row, col)) throw new IllegalArgumentException();
+        if (!isOpen(row, col)) return isOpen(row, col);
+
+        int site = sitesWithoutBottomConnection.find(getIndex(row, col));
+        int top = sitesWithoutBottomConnection.find(0);
+        return site == top;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        if (n == 1) return isOpen(1, 1);
+        int top = sites.find(0);
+        int bottom = sites.find(n * n + 1);
+        return top == bottom;
+    }
 }
--- Part 1/Week 1/PercolationStats.java
+++ Part 1/Week 1/PercolationStats.java
@@ -3,63 +3,69 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-	private final int trials;
-	private double[] result;
-	private boolean meanFlag = false;
-	private double mean;
-	private boolean stddevFlag = false;
-	private double stddev;
-	private final int n;
 
-	public PercolationStats(int n, int trials) {
-		if (n <= 0 || trials <= 0) {
-			throw new IllegalArgumentException();
-		}
-		result = new double[trials];
-		this.trials = trials;
-		this.n = n;
-	}
+    private static final double CONFIDENCE_95 = 1.96;
 
-	public double mean() {
-		if (meanFlag) {
-			return mean;
-		}
-		Percolation grid = null;
-		for (int i = 0; i < trials; i++) {
-			grid = new Percolation(n);
-			while (!grid.percolates()) {
-				grid.open(StdRandom.uniform(1, n + 1), StdRandom.uniform(1, n + 1));
-			}
-			result[i] = (double) grid.numberOfOpenSites() / (n * n);
-		}
-		mean = StdStats.mean(result);
-		meanFlag = true;
-		return mean;
-	}
+    private boolean meanFlag = false;
+    private double mean;
+    private boolean stddevFlag = false;
+    private double stddev;
 
-	public double stddev() {
-		mean();
-		if (stddevFlag) {
-			return stddev;
-		} else {
-			stddevFlag = true;
-			stddev = StdStats.stddev(result);
-			return stddev;
-		}
-	}
+    private final int trials;
+    private final double[] result;
+    private final int n;
 
-	public double confidenceLo() {
-		return mean() - 1.96 * stddev() / Math.sqrt(trials);
-	}
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException();
+        }
+        result = new double[trials];
+        this.trials = trials;
+        this.n = n;
+    }
 
-	public double confidenceHi() {
-		return mean() + 1.96 * stddev() / Math.sqrt(trials);
-	}
+    public double mean() {
+        if (meanFlag) {
+            return mean;
+        }
+        Percolation grid;
+        for (int i = 0; i < trials; i++) {
+            grid = new Percolation(n);
+            while (!grid.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                grid.open(row, col);
+            }
+            result[i] = (double) grid.numberOfOpenSites() / (n * n);
+        }
+        mean = StdStats.mean(result);
+        meanFlag = true;
+        return mean;
+    }
 
-	public static void main(String[] args) {
-		PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
-		System.out.println("mean\t\t\t= " + stats.mean());
-		System.out.println("stddev\t\t\t= " + stats.stddev());
-		System.out.println("95% confidence interval\t= [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
-	}
+    public double stddev() {
+        mean();
+        if (stddevFlag) {
+            return stddev;
+        } else {
+            stddevFlag = true;
+            stddev = StdStats.stddev(result);
+            return stddev;
+        }
+    }
+
+    public double confidenceLo() {
+        return mean() - CONFIDENCE_95 * stddev() / Math.sqrt(trials);
+    }
+
+    public double confidenceHi() {
+        return mean() + CONFIDENCE_95 * stddev() / Math.sqrt(trials);
+    }
+
+    public static void main(String[] args) {
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean\t\t\t= " + stats.mean());
+        System.out.println("stddev\t\t\t= " + stats.stddev());
+        System.out.println("95% confidence interval\t= [" + stats.confidenceLo() + ", " + stats.confidenceHi() + "]");
+    }
 }
